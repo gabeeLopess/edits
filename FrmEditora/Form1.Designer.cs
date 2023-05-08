c71145062784a170bcd09da582d36e62a0fc554b
Gabriel Lopes <108100283+gabeeLopess@users.noreply.github.com>
2023-05-08 10:02:35 -0300

    Add files via upload

---
+++ FrmEditora/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FrmEditora
+{
+	partial class FrmEditora
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.gridLayout = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridLayout)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(107, 54);
+            this.lblCodigo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(40, 13);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Código";
+            this.lblCodigo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(112, 99);
+            this.lblNome.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome";
+            this.lblNome.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(218, 54);
+            this.txtCodigo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(76, 20);
+            this.txtCodigo.TabIndex = 2;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(218, 94);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(226, 20);
+            this.txtNome.TabIndex = 3;
+            this.txtNome.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(253, 152);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(68, 28);
+            this.btnSalvar.TabIndex = 4;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click_1);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(343, 152);
+            this.btnExcluir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(68, 28);
+            this.btnExcluir.TabIndex = 5;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click_1);
+            // 
+            // gridLayout
+            // 
+            this.gridLayout.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridLayout.Location = new System.Drawing.Point(218, 198);
+            this.gridLayout.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gridLayout.Name = "gridLayout";
+            this.gridLayout.RowHeadersWidth = 51;
+            this.gridLayout.RowTemplate.Height = 24;
+            this.gridLayout.Size = new System.Drawing.Size(233, 156);
+            this.gridLayout.TabIndex = 6;
+            this.gridLayout.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridLayout_CellContentClick);
+            this.gridLayout.DoubleClick += new System.EventHandler(this.gridLayout_DoubleClick);
+            // 
+            // FrmEditora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 376);
+            this.Controls.Add(this.gridLayout);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblCodigo);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FrmEditora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Editora";
+            this.Load += new System.EventHandler(this.FrmEditora_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridLayout)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		
+		#endregion
+
+		private System.Windows.Forms.Label lblCodigo;
+		private System.Windows.Forms.Label lblNome;
+		private System.Windows.Forms.TextBox txtCodigo;
+		private System.Windows.Forms.TextBox txtNome;
+		private System.Windows.Forms.Button btnSalvar;
+		private System.Windows.Forms.Button btnExcluir;
+		private System.Windows.Forms.DataGridView gridLayout;
+	}
+}
+
---
+++ FrmEditora/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace FrmEditora
+{
+	public partial class FrmEditora : Form
+	{
+		SqlConnection conexao;
+		SqlCommand comando;
+		SqlDataAdapter da;
+		string strSQL;
+		// Variáveis que controlam o ID editora
+
+		private int ultimoID;
+		private int proximoID;
+        // Variável para minhasecao
+        private bool secaoCadastrada;
+        public FrmEditora()
+		{
+			InitializeComponent();
+			edit_Load();
+		}
+
+		private SqlConnection CriarConexao()
+		{
+			conexao = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=Treinamento;Integrated Security=True");
+			return conexao;
+		}
+
+        private void AtualizarBotoesExcluir()
+        {
+            if (secaoCadastrada)
+            {
+                // Se a seção estiver cadastrada, mostrar o botão de excluir
+                btnExcluir.Visible = true;
+            }
+            else
+            {
+                // Se a seção não estiver cadastrada, ocultar o botão de excluir
+                btnExcluir.Visible = false;
+            }
+        }
+        private void btnSalvar_Click_1(object sender, EventArgs e)
+		{
+
+		
+		conexao = CriarConexao();
+
+			try
+			{
+                //uso a variavel para armazenar meu id que seria o codEditora
+                int id = Convert.ToInt32(txtCodigo.Text);
+
+
+				if (id > ultimoID)
+				{
+
+
+					strSQL = "INSERT INTO MvtBIBEditora (NOME) VALUES (@NOME)";
+					comando = new SqlCommand(strSQL, conexao);
+					comando.Parameters.AddWithValue("@NOME", txtNome.Text);
+					conexao.Open();
+					comando.ExecuteNonQuery();
+
+					MessageBox.Show("Salvo com sucesso");
+
+                    //passando o metodo de obter o ultimo id a variavel
+                    ultimoID = ObterUltimoID();
+                    //passando um valor a mais para minha variavel id que ira ser igual a minha lbl,
+                    //ent agr em minha lbl se eu tinha 1 la, agr irei ter 2, e assim vai
+                    proximoID = ultimoID + 1;
+					id++;
+                    txtCodigo.Text = id.ToString();
+                    //limpando
+                    txtNome.Text = string.Empty;
+
+
+					edit_Load();
+				}
+				else
+				{
+                    //como ja existe, ira fazer update, dessa forma atualizando.
+                    strSQL = "UPDATE MvtBIBEditora  SET NOME =  @NOME WHERE codEditora = @CodEditora";
+					comando = new SqlCommand(strSQL, conexao);
+					comando.Parameters.AddWithValue("@NOME", txtNome.Text);
+					comando.Parameters.AddWithValue("@CodEditora", txtCodigo.Text);
+					conexao.Open();
+					comando.ExecuteNonQuery();
+
+					MessageBox.Show("Atualizado com sucesso");
+                    //passando um valor a mais para minha variavel id que ira ser igual a minha lbl,
+                    //isso funciona tb para atualizar, se eu atualizei o id 1, agr ira aparecer o id 2 em minha lbl
+                    id++;
+                    //limpando
+                    txtCodigo.Text = id.ToString();
+					txtNome.Text = string.Empty;
+                    secaoCadastrada = false; // Inicialmente, a seção não foi cadastrada entao escondo meu botao
+                     //metodo atualizar btn excluir						
+                    AtualizarBotoesExcluir();
+                    edit_Load();
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			finally
+			{
+				conexao.Close();
+
+			}
+
+		}
+
+		private void btnExcluir_Click_1(object sender, EventArgs e)
+		{
+
+		conexao = CriarConexao();
+
+			try
+			{
+				int id = Convert.ToInt32(txtCodigo.Text);
+
+				strSQL = "DELETE MvtBIBEditora WHERE codEditora = @CodEditora";
+				comando = new SqlCommand(strSQL, conexao);
+				comando.Parameters.AddWithValue("@CodEditora", txtCodigo.Text);
+				conexao.Open();
+				comando.ExecuteNonQuery();
+
+				MessageBox.Show("Excluido com sucesso");
+
+                //mando para minha lbl o proximo id que deveras ser cadastrado
+                txtCodigo.Text = proximoID.ToString();
+                txtNome.Text = string.Empty;
+                secaoCadastrada = false; //Inicialmente, a seção não foi cadastrada entao escondo meu botao
+                //metodo atualizar btn excluir	
+                AtualizarBotoesExcluir();
+                edit_Load();
+
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			finally
+			{
+				conexao.Close();
+				comando.Clone();
+
+			}
+
+
+		}
+
+		private void edit_Load()
+		{
+			conexao = CriarConexao();
+
+			try
+			{
+				strSQL = "SELECT codEditora AS ID, nome AS NOME FROM MvtBIBEditora";
+
+
+				DataSet ds = new DataSet();
+				da = new SqlDataAdapter(strSQL, conexao);
+
+				conexao.Open();
+				da.Fill(ds);
+				gridLayout.DataSource = ds.Tables[0];
+
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+
+		}
+
+		private void FrmEditora_Load(object sender, EventArgs e)
+		{
+			//conexão com o banco de dados
+			conexao = CriarConexao();
+			conexao.Open();
+            //passando o metodo de obter o ultimo id para uma variavel
+            ultimoID = ObterUltimoID();
+            //uso essa varaivel para obter o proximo id, usando a ultima variavel + 1
+            proximoID = ultimoID + 1;
+            //passo isso a minha label
+            txtCodigo.Text = proximoID.ToString();
+            secaoCadastrada = false; // Inicialmente, a seção não foi cadastrada entao escondo meu botao
+            //metodo atualizar btn excluir
+            AtualizarBotoesExcluir();
+
+            conexao.Close();
+		}
+
+		private int ObterUltimoID()
+		{
+            //comando SQL  que me permite obter o maior valor presente na coluna codEditora
+            string query = "SELECT MAX(codEditora) FROM MvtBIBEditora";
+			using (SqlCommand command = new SqlCommand(query, conexao))
+			{
+
+				object result = command.ExecuteScalar();
+                //verifica se o resultado da consulta SQL é diferente de um id inexistente
+                if (result != DBNull.Value)
+				{
+					return Convert.ToInt32(result);
+				}
+				else
+				{
+					return 0;
+				}
+			}
+		}
+
+		private void gridLayout_DoubleClick(object sender, EventArgs e)
+		{
+
+		
+			try
+			{
+
+				if (gridLayout.SelectedRows.Count >= 0)
+				{
+                    //aqui é o unico lugar onde irei mostrar meu botao excluir,
+                    //pois para eu poder dar double click devo ter algum registro cadastrado
+                    txtCodigo.Text = gridLayout.SelectedRows[0].Cells[0].Value.ToString();
+					txtNome.Text = gridLayout.SelectedRows[0].Cells[1].Value.ToString();
+                    btnExcluir.Visible = true;
+
+                }
+
+			}
+			catch (Exception ex)
+			{
+
+			}
+		}
+		
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click_1(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+        private void gridLayout_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
